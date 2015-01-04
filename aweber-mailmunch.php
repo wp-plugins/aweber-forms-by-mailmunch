254485c6ec38ecf9a508695d7d619b42681d4926
mailmunch <mailmunch@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-04 13:13:27 +0000

    fixed the bug where user gets signed out - version 1.0.5
    git-svn-id: https://plugins.svn.wordpress.org/aweber-forms-by-mailmunch/trunk@1059504 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- aweber-mailmunch.php
+++ aweber-mailmunch.php
@@ -3,7 +3,7 @@
   Plugin Name: AWeber Forms by MailMunch
   Plugin URI: http://www.mailmunch.co
   Description: The AWeber plugin allows you to quickly and easily add signup forms for your AWeber lists. Popup, Embedded, Top Bar and a variety of different options available.
-  Version: 1.0.4
+  Version: 1.0.5
   Author: MailMunch
   Author URI: http://www.mailmunch.co
   License: GPL2
@@ -14,7 +14,7 @@
   require_once( plugin_dir_path( __FILE__ ) . 'inc/sidebar_widget.php' );
 
   define( 'AWEBER_MAILMUNCH_SLUG', "aweber-mailmunch");
-  define( 'AWEBER_MAILMUNCH_VER', "1.0.4");
+  define( 'AWEBER_MAILMUNCH_VER', "1.0.5");
   define( 'AWEBER_MAILMUNCH_URL', "www.mailmunch.co");
 
   // Create unique WordPress instance ID
@@ -322,7 +322,7 @@
         <div class="form-group">
           <input type="hidden" name="action" value="save_settings" />
 
-          <select name="mailmunch_data[site_id]">
+          <select name="aw_mm_data[site_id]">
             <?php foreach ($sites as $site) { ?>
             <option value="<?php echo $site->id ?>"><?php echo $site->name ?></option>
             <?php } ?>
